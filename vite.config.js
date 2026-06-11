8cab03b8b7cefde80111473dbfa85b50be1ba556
salahesam <salahesam111@gmail.com>
2026-06-11 16:05:09 +0300

    Performance optimizations for PageSpeed 90+

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    cssMinify: true
+  },
   test: {
     environment: 'jsdom',
     globals: true,
